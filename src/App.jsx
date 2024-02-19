8f741656b7d612ca0f6c43a3e6db25735d1d3715
avengsum <sy710481@gmail.com>
2024-02-19 11:09:01 +0530

    intial app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@chakra-ui/react'
+
+function App() {
+  
+  return (
+    <Container maxW="620px">
+    <button>Hello</button>
+    </Container>
+  )
+}
+
+export default App
